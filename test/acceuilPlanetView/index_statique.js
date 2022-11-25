f31d6f8c9c4d79a8328c22dcc99b42cb1c6c8d3e
Fabien Rouchon <hikem1@hotmail.fr>
2022-11-25 15:48:49 +0100

    add acceuil statique / fabien

---
+++ test/acceuilPlanetView/index_statique.js
@@ -0,0 +1,29 @@
+import  { gsap }  from '/node_modules/gsap/index.js';
+
+const infoDivEl = document.querySelector('#info');
+const cardContainer = document.querySelector('#card');
+const homeIconEl = document.querySelector('#homeIcon'); 
+
+homeIconEl.addEventListener('click', ()=> {
+    infoDivEl.style.backgroundImage = 'url("assets/img/captureAcceuilPlanet.png")';
+    cardEffect('origin');
+    console.log('click i');
+})
+
+cardContainer.addEventListener('click', ()=> {
+    infoDivEl.style.backgroundImage = 'url("assets/img/capturePlanetSelected.png")';
+    cardEffect('example');
+    console.log('click card');
+})
+
+
+function cardEffect(planetName) {
+    if(planetName != 'origin'){
+      gsap.fromTo(".card-container", { opacity:0},{opacity:1,delay:2.1});
+      gsap.fromTo("h1", { x:300},{x:0,delay:2.3});
+      gsap.fromTo(".card-body", { opacity:0},{opacity:1,delay:2.7});
+    }
+    else{
+      gsap.fromTo(".card-container", { opacity:1},{opacity:0,duration:0.5});
+    }
+  };
