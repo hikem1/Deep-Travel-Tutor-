0e0ebb9fe3f8a5b6bb68625f649347a9fd01ba8b
Fabien Rouchon <hikem1@hotmail.fr>
2022-12-07 15:35:54 +0100

    reorganized project

--- partial/header.php
+++ partial/header.php
@@ -4,16 +4,13 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="header.css">
         <link href="http://fonts.cdnfonts.com/css/copperplate" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+        <link rel="stylesheet" href="header.css">
         <script src="header.js" defer></script>
         <title>Deep Travel</title>
     </head>
     <body>
-
-
-
 
 <!-- ________________________________________________________sidebar______________________________________________ -->
 
@@ -48,8 +45,6 @@
                     </div>
                 </div>
 
-
-
                 <div>
                     <div class="sidenav-link links">
                         <i id="i-langue" class="fa fa-chevron-right" aria-hidden="true"></i><a id="drop-link-langue" class="">Langue</a>
@@ -68,49 +63,37 @@
             </nav>
         </div>
 
-
-
-
-
-        <div id="main">
 <!-- ______________________________________________________header_____________________________________________________ -->
 
-            <header>
-                <img class="logo links" src="/assets/img/logoDetoureComp.png" alt="">
-                <nav>
-                    <a >Acceuil</a>
-                    <a >Destinations</a>
-                    <a >Nos vaisseaux</a>
-                    <a >Vie à bord</a>
-                    <a >Informations</a>
-                </nav>
-                <div id="icons-login-language">
-                    <div>
-                        <i id="login-icon" class="fa-solid fa-user-astronaut fa-xl links"></i>
-                        <div id="dropdown-login" class="d-none">
-                            <div class="dropdown-link links"><i class="fa fa-chevron-right" aria-hidden="true"></i><a>Connexion</a>
-                            </div>
-                            <div class="dropdown-link links"><i class="fa fa-chevron-right" aria-hidden="true"></i><a>Mon Compte</a>
-                            </div>
+        <header>
+            <img class="logo links" src="/assets/img/logoDetoureComp.png" alt="">
+            <nav>
+                <a >Acceuil</a>
+                <a >Destinations</a>
+                <a >Nos vaisseaux</a>
+                <a >Vie à bord</a>
+                <a >Informations</a>
+            </nav>
+            <div id="icons-login-language">
+                <div>
+                    <i id="login-icon" class="fa-solid fa-user-astronaut fa-xl links"></i>
+                    <div id="dropdown-login" class="d-none">
+                        <div class="dropdown-link links"><i class="fa fa-chevron-right" aria-hidden="true"></i><a>Connexion</a>
                         </div>
-                    </div>
-                    <div>
-                        <i id="langue-icon" class="fa-solid fa-earth-oceania fa-xl links"></i>
-                        <div id="dropdown-language" class="d-none">
-                            <img class="links" src="assets/icon/france.png" alt=""></img>
-                            <img class="links" src="assets/icon/united-kingdom.png" alt=""></img>
+                        <div class="dropdown-link links"><i class="fa fa-chevron-right" aria-hidden="true"></i><a>Mon Compte</a>
                         </div>
                     </div>
                 </div>
-                <div id="icon-burger">
-                    <i id="btn-burger" class="fa fa-bars fa-2x links" aria-hidden="true"></i>
+                <div>
+                    <i id="langue-icon" class="fa-solid fa-earth-oceania fa-xl links"></i>
+                    <div id="dropdown-language" class="d-none">
+                        <img class="links" src="assets/icon/france.png" alt=""></img>
+                        <img class="links" src="assets/icon/united-kingdom.png" alt=""></img>
+                    </div>
                 </div>
-            </header>
+            </div>
+            <div id="icon-burger">
+                <i id="btn-burger" class="fa fa-bars fa-2x links" aria-hidden="true"></i>
+            </div>
+        </header>
 
-
-
-            
-
-        </div>
-    </body>
-</html>
