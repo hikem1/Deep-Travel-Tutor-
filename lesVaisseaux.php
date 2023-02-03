f6cdc1de3e33caae8f53b79cfa586530828327b8
jean <pokhemidy@gmail.com>
2023-02-03 13:17:13 +0100

    vaisseau

--- lesVaisseaux.php
+++ lesVaisseaux.php
@@ -1,6 +1,180 @@
 <?php
-
+$page = "vaisseau";
 include "./partial/header.php"
 
 
 ?>
+ <div class="figures-vaisseaux">
+          <div class="contour">
+            <h2>moya</h2>
+            <div class="container">
+              <div class="infos">
+                <p>nombre de place restant</p>
+              </div>
+              <div class="time">
+                <!-- <time>27/11/2022</time> -->
+              </div>
+              <div class="figure-vaisseau">
+                <img src="assets\img\vaisseau\ship.png" alt="moya" class="moya">
+              </div>
+              <div class="figure-vaisseau-bis">
+                <p>
+                  Moteur à fusion nucléaire : deux atomes légers (principalement
+                  l'hydrogène) fusionnent:<br />
+                  grâce à une amorce énergétique pour former des composés plus
+                  lourds avec un dégagement d’énergie encore plus puissant.<br />
+                  On pense que ce serait le système le plus économique.<br />
+                  Son carburant, l’hydrogène, est abondant dans l’espace et donc
+                  quasiment gratuit.<br />
+                  Pour en récolter une quantité suffisante, le vaisseau pourrait
+                  avoir une forme d’entonnoir et un diamètre supérieur à 40 km.<br />
+                </p>
+              </div>
+            </div>
+  
+            <div class="container-2">
+              <div class="détail-vaisseaux">
+                <img src="assets\img\vaisseau\intlune.jpg" alt="interieur vaisseau lune" />
+              </div>
+              <div class="bar">
+                <img src="assets\img\vaisseau\barlune.jpg" alt="bar vaisseau lune" />
+              </div>
+            </div>
+  
+            <div class="btn-lien">
+              <button><a href=""> lien destination </a></button>
+            </div>
+            <div class="btn-lien2">
+              <button><a href="">lien vie à bord </a></button>
+            </div>
+          </div>
+        </div>
+        <div class="figures-vaisseaux">
+            <div class="contour1">
+              <h2>Arcadia</h2>
+              <div class="container">
+                <div class="infos">
+                  <p>nombre de place restant</p>
+                </div>
+                <div class="time">
+                  <!-- <time>27/11/2022</time> -->
+                </div>
+                <div class="figure-vaisseau">
+                    <img src="assets\img\vaisseau\VaisseauTwo(1).png" alt="vaisseau spacial" class="arcadia"/>
+                </div>
+                <div class="figure-vaisseau-bis">
+                    <p>
+                      Moteur à fusion nucléaire : deux atomes légers (principalement
+                      l'hydrogène) fusionnent:<br />
+                      grâce à une amorce énergétique pour former des composés plus
+                      lourds avec un dégagement d’énergie encore plus puissant.<br />
+                      On pense que ce serait le système le plus économique.<br />
+                      Son carburant, l’hydrogène, est abondant dans l’espace et donc
+                      quasiment gratuit.<br />
+                      Pour en récolter une quantité suffisante, le vaisseau pourrait
+                      avoir une forme d’entonnoir et un diamètre supérieur à 40 km.<br />
+                    </p>
+                  </div>
+                </div>
+                <div class="container-2">
+                    <div class="détail-vaisseaux">
+                      <img src="assets\img\vaisseau\intmars.jpg" alt="interieur vaisseau" />
+                    </div>
+                    <div class="bar">
+                      <img src="assets\img\vaisseau\barmars.jpg" alt="bar vaisseau" />
+                    </div>
+                  </div>
+                  <div class="btn-lien">
+                    <button><a href=""> lien destination </a></button>
+                  </div>
+                  <div class="btn-lien2">
+                    <button><a href="">lien vie à bord </a></button>
+                  </div>
+                </div>
+                <div class="figures-vaisseaux">
+                    <div class="contour2">
+                      <h2>Serenity</h2>
+                      <div class="container">
+                        <div class="infos">
+                          <p>nombre de place restant</p>
+                        </div>
+                        <div class="time">
+                          <!-- <time>27/11/2022</time> -->
+                        </div>
+                        <div class="figure-vaisseau">
+                            <img src="assets\img\vaisseau\vaisseauvenus.png" alt="vaisseau spacial venus"  class="Serenity"/>
+                          </div>
+                          <div class="figure-vaisseau-bis">
+                            <p>
+                              Moteur à fusion nucléaire : deux atomes légers (principalement
+                              l'hydrogène) fusionnent:<br />
+                              grâce à une amorce énergétique pour former des composés plus
+                              lourds avec un dégagement d’énergie encore plus puissant.<br />
+                              On pense que ce serait le système le plus économique.<br />
+                              Son carburant, l’hydrogène, est abondant dans l’espace et donc
+                              quasiment gratuit.<br />
+                              Pour en récolter une quantité suffisante, le vaisseau pourrait
+                              avoir une forme d’entonnoir et un diamètre supérieur à 40 km.<br />
+                            </p>
+                          </div>
+                        </div>
+                        <div class="container-2">
+                            <div class="détail-vaisseaux">
+                              <img src="assets\img\vaisseau\intvenus.jpg"  alt="interieur vaisseau lune" />
+                            </div>
+                            <div class="bar">
+                              <img src="assets\img\vaisseau\barvenus.jpg" alt="bar vaisseau lune" />
+                            </div>
+                          </div>
+                          <div class="btn-lien">
+                            <button><a href=""> lien destination </a></button>
+                          </div>
+                          <div class="btn-lien2">
+                            <button><a href="">lien vie à bord </a></button>
+                          </div>
+                        </div>
+                        <div class="figures-vaisseaux">
+                            <div class="contour3">
+                              <h2>Avalon</h2>
+                              <div class="container">
+                                <div class="infos">
+                                  <p>nombre de place restant</p>
+                                </div>
+                                <div class="time">
+                                  <!-- <time>27/11/2022</time> -->
+                                </div>
+                                <div class="figure-vaisseau">
+                                    <img src="assets\img\vaisseau\spaceship.png" alt="vaisseau spacial venus" class="Avalon"/>
+                                </div>
+                                <div class="figure-vaisseau-bis">
+                                  <p>
+                                    Moteur à fusion nucléaire : deux atomes légers (principalement
+                                    l'hydrogène) fusionnent:<br />
+                                    grâce à une amorce énergétique pour former des composés plus
+                                    lourds avec un dégagement d’énergie encore plus puissant.<br />
+                                    On pense que ce serait le système le plus économique.<br />
+                                    Son carburant, l’hydrogène, est abondant dans l’espace et donc
+                                    quasiment gratuit.<br />
+                                    Pour en récolter une quantité suffisante, le vaisseau pourrait
+                                    avoir une forme d’entonnoir et un diamètre supérieur à 40 km.<br />
+                                  </p>
+                                </div>
+                              </div>
+                              <div class="container-2">
+                                <div class="détail-vaisseaux">
+                                  <img src="assets\img\vaisseau\intmercure.jpg"   alt="interieur vaisseau lune" />
+                                </div>
+                                <div class="bar">
+                                  <img src="assets\img\vaisseau\barmercure.jpg"  alt="bar vaisseau lune" />
+                                </div>
+                              </div>
+                              <div class="btn-lien">
+                                <button><a href=""> lien destination </a></button>
+                              </div>
+                              <div class="btn-lien2">
+                                <button><a href="">lien vie à bord </a></button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <script src="script.js" defer></script>
