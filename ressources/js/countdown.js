b3468b247766b955d7186aa251b8c1983908d54f
JhSalmon <jimtisswood@hotmail.fr>
2023-02-13 13:58:31 +0100

    commentaireCountdownFunction

--- ressources/js/countdown.js
+++ ressources/js/countdown.js
@@ -1,20 +1,23 @@
 const countDownEl = document.querySelector('.card-footer h2');
-
+//la seclection de l'élement où s'affiche le compte à rebour.
 const oneDayMs = 1000 * 60 * 60 * 24;
 const oneHourMs = 1000 * 60 * 60;
 const oneMinuteMs = 1000 * 60;
 const oneSecondeMs = 1000;
-
+// la convertion du temps en millis secondes.
 let targetDate = '';
+//pour effacer l'affichage de la date.
 let countDownId ='';
+// pour afficher l'intervalle
 
 function countdown(stageDate) {
 
     let dateNow = new Date();
     
     let timeLeft = stageDate - dateNow ;
+    // le temps restant entre la date de mainteant et la date prèvu.
     let daysLeft = Math.floor(timeLeft / oneDayMs);
-    
+    // ça compte le nombre de jours de restant.
     let hoursLeftMs = timeLeft - ( daysLeft * oneDayMs ) ;
     let hoursLeft = Math.floor(hoursLeftMs / oneHourMs);
     
